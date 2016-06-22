655bbbe3b0cf85c9d1b55c997e6bbc5bd27d4670
Melanie Heisey <mheisey.nox@gmail.com>
2016-06-22 14:11:37 -0700

    fit rfmodel without nonneural papers

--- classifyinteresting.R
+++ classifyinteresting.R
@@ -18,17 +18,20 @@ con = dbConnect(driver, dbname=db,
                 password = pw)
 
 
-dbarxivs = dbGetQuery(con, "SELECT note, url FROM link WHERE url LIKE '%arxiv%'")
+# take non-neural stuff
+dbarxivs = dbGetQuery(con, "SELECT link.note, link.url FROM link
+LEFT JOIN tag ON link.note = tag.note
+WHERE link.url LIKE '%arxiv%' AND NOT tagname LIKE '%non%'")
 # try to parse these
 library("stringr")
-dbarxivs$uri = str_match(dbarxivs$url, "arxiv.org/.../(\\d\\d\\d\\d[.]\\d\\d\\d\\d\\d?)")[,2]
+dbarxivs$uri = str_match(dbarxivs$url, "arxiv.org/.../(\\d\\d\\d\\d[.]\\d\\d\\d\\d\\d?)(?:v\\d)?(?:[.]pdf)?$")[,2]
 
 # look at what's going on
-dbarxivs[is.na(dbarxivs$uri),1:2]
-# remove arxiv/bulk_data and arxiv-sanity
-dbarxivs = dbarxivs[-c(230,325),]
+dbarxivs[is.na(dbarxivs$uri),1:3]
+dbarxivs[4018:4019,3] = "1604.01277"
+dbarxivs = dbarxivs[-c(404,536),]
 
-# 58 duplicates
+# 3053 duplicates
 nrow(dbarxivs) - length(unique(dbarxivs$uri))
 
 
@@ -43,25 +46,24 @@ dbUnloadDriver(driver)
 # label as in-interesting or not
   # check my db pull against arxiv pull
   sum(!dbarxivs$uri %in% abs$arxivid)
-  mismatch = dbarxivs[!dbarxivs$uri %in% abs$arxivid,]
+  dbarxivs[!dbarxivs$uri %in% abs$arxivid,]
   
   # get url for more
-  paste0("http://export.arxiv.org/api/query?search_query=",
-    paste0("id:", mismatch$uri, collapse=" OR "),
-  "&start=0&max_results=30")
+  # paste0("http://export.arxiv.org/api/query?search_query=",
+  #   paste0("id:", mismatch$uri, collapse=" OR "),
+  # "&start=0&max_results=30")
 
   # check against my db pull
   abs$interest = abs$arxivid %in% dbarxivs$uri
   # check against lahwran's gist positive examples
   temp = read.delim("metadata/names_positive_examples",colClasses = "character", header=FALSE)
   
-  # 4 differences
+  # 8 differences, from non-neural, except 15.05008
   sum(abs$arxivid %in% temp$V1 != abs$interest)
   abs$arxivid[abs$arxivid %in% temp$V1 != abs$interest]
-    # interesting, my list doesn't have them. Indeed, they are not in there as links so I do not. I'll go fix 3/4 of those now
 
  # count the confusion matrix for base rate
- table(abs$interest) # 1626 false, 1314 true. 55.3% baseline
+ table(abs$interest) # 1633 false, 1327 true. 44.83% baseline
 
 # get document term matrix for summary
 library(tm)
@@ -76,7 +78,7 @@ corpus <- tm_map(corpus, stripWhitespace)
 BigramTokenizer <- function(x) {
   unlist(lapply(ngrams(words(x), 2), paste, collapse = " "), use.names = FALSE)}
 
-set.seed(461)
+set.seed(3506)
 ttsplit = (sample(seq(corpus), length(corpus)) %% 20) == 7
 testcorp = corpus[ttsplit]
 traincorp = corpus[!ttsplit]
@@ -86,20 +88,14 @@ testabs = abs[ttsplit,]
 frequencies = DocumentTermMatrix(traincorp, control=list(tokenize = "words"))
 #frequencies2 = DocumentTermMatrix(traincorp, control=list(tokenize = BigramTokenizer))
 frequencies
-# check for zero variances:
-#tvar = apply(frequencies[trainabs$interest,], 2, var)
-#fvar = apply(frequencies[!trainabs$interest,], 2, var)
-length(tvar[tvar > 0 & fvar > 0])
 
-#frequencies.sparse = frequencies[,tvar > 0 & fvar > 0]
-frequencies.sparse = removeSparseTerms(frequencies, 0.90)
+# visualize cutoff points
+sparseness = colSums(as.matrix(frequencies))/nrow(frequencies)
+sparseness = data.frame(x=seq(0,1,.01), y=sapply(seq(0,1,.01), function(x) sum(sparseness > x)))
+ggplot(sparseness[2:40,], aes(x=x, y=y)) + geom_point()
 
-# dictionary=charactervector
- #http://www.r-bloggers.com/text-mining-the-complete-works-of-william-shakespeare/
-# dummy categories
-# 3,8,5,3,4
-#abs$categoryterms[c(31,47,49,54,56)]
-#str_match_all(abs$categoryterms[c(31,47,49,54,56)], "[{,;\" ]+(.+?)(?=[ },\"]+)")
+frequencies.sparse = removeSparseTerms(frequencies, 0.93)
+
 
 # helper function
 ragged_match = function(ragged_list) {
@@ -115,37 +111,23 @@ ragged_match = function(ragged_list) {
   return(results)
 }
 
-# catterms = str_match_all(abs$categoryterms, "[{,;\" (]+(.+?)(?=[ ;},\")]+)")
-#catterms = str_match_all(abs$categoryterms, "[{,](.+?)(?=[},])|[\",;] ?((\d\d[-A-Z])[0-9x][0-9x])(?=[\",;])")
-
 catterms = str_match_all(trainabs$categoryterms, "[{\",; ]((([A-Z])\\.[0-9]*)(?:\\.[0-9m]*)?)(?:\\.[0-9a-z])?(?=[ \",;}])|[{\",; ](((\\d\\d)[-A-Z])[0-9xX][0-9xX])(?=[ }\",;])|[{, ](([-a-zA-Z]*)(?:\\.[-a-zA-Z]*)?)(?=[ },])")
 
 categories = ragged_match(catterms)
 # 1602.01323 (row 1506) needs to add to 68U15, was mistyped as 6U815
 seq_len(nrow(trainabs))[grepl("6U8",trainabs$categoryterms)]
-categories[1425,"68U"] = 1
-categories[1425,"68"] = 1
+categories[1430,"68U"] = 1
+categories[1430,"68"] = 1
 # remove Primary, Secondary, ""
 categories = categories[,!colnames(categories) %in% c("theory",
                         "Secondary", "")]
-dim(categories) # 461 now
-sparseness = colSums(categories) / nrow(categories)
-categories = categories[,sparseness > .01]
-# misccategory = rowSums(categories[,colSums(categories) <= 3])
-# categories = cbind(categories[,colSums(categories) > 3], othercategory=misccategory)
-# 
-# # check for zero-variances:
-# splitty = split(as.data.frame(as.matrix(categories)), trainabs$interest)
-# # True variances
-# tvar = apply(splitty$`TRUE`, 2, var)
-# categories = categories[,tvar > 0]
-# 
-# splitty = split(as.data.frame(as.matrix(categories)), trainabs$interest)
-# false variances
-# fvar = apply(splitty$`FALSE`, 2, var)
-# fvar[fvar < 5/nrow(categories)]
-# categories = categories[, fvar > 0]
+dim(categories) # 456 now
 
+# visualize cutoff points
+sparseness = colSums(categories) / nrow(categories)
+plotty = data.frame(x=seq(0,.1,.001), y=sapply(seq(0,.1,.001), function(x) sum(sparseness > x)))
+ggplot(plotty[1:20,], aes(x=x, y=y)) + geom_point()
+categories = categories[,sparseness > 0.003] # 79 categories
 
 
 library("Matrix")
@@ -168,45 +150,41 @@ library("Matrix")
   ggplot(data.frame(), aes(x=colSums(authors))) + geom_histogram()
   # 3 to 5 looks quite good, going to check how many actual papers that eliminates
 
-  # # check for zero-variances:
-  # splitty = split(as.data.frame(as.matrix(authors)), trainabs$interest)
-  # tvar = apply(splitty$`TRUE`, 2, var)
-  # fvar = apply(splitty$`FALSE`, 2, var)
-  # authors = authors[, tvar > 0 & fvar > 0]
-
 sparseness = colSums(authors) / nrow(authors)
-authors = authors[,sparseness > .004]
+plotty = data.frame(x=seq(0,.1,.001), y=sapply(seq(0,.1,.001), function(x) sum(sparseness > x)))
+ggplot(plotty[2:20,], aes(x=x, y=y)) + geom_point()
+authors = authors[,sparseness > 0.002] # 85 categories
+
 # prepend dummied categories, authors
-  #colnames(categories) = paste0("category-",colnames(categories))
   inputvars = cbind(as.matrix(authors),categories, as.matrix(frequencies.sparse))
   # make sure no duplicated names
   sum(duplicated(c(colnames(authors), colnames(categories), colnames(frequencies.sparse))))
 
-  #inputvars = inputvars[,colnames(inputvars) != ""]
-  #inputvars = inputvars[,colnames(inputvars) != "category-"]
-
-  save(categories, authors, trainabs, testabs, abs, dbarxivs, frequencies, frequencies.sparse, file = "savestuff.rdata")
 # naivebayes simple run
   library("klaR")
   # remove zero variances
   tval = apply(inputvars[trainabs$interest,], 2, var)
   fval = apply(inputvars[!trainabs$interest,], 2, var)
+  # 30 to be removed
   naiveinput = inputvars[,tval > 0 & fval > 0]
   
   model = NaiveBayes(naiveinput, factor(trainabs$interest))
-  model2 = NaiveBayes(as.matrix(frequencies.sparse), factor(trainabs$interest))
-  #modelsmooth = NaiveBayes(inputvars, factor(trainabs$interest), fL=.5)
   # get predictions matrix: is it better than base rate?
   results = predict(model)
 
 library("pROC")
 roc(trainabs$interest, results$posterior[,1], plot=TRUE)
-# #0.833... #.8211 for simplified version
+#0.7914
 roc(trainabs$interest, results$posterior[,2], plot=TRUE)
-# #0.8376... #.8234 for simplified version
+#0.7993
 
 
-sum((results$class == "TRUE") == trainabs$interest)/nrow(trainabs) # accuracy 63.9%.... #0.7816501 for simplified version
+sum((results$class == "TRUE") == trainabs$interest)/nrow(trainabs) # 76.81%
+
+accuracy = sapply(seq(0,1,.05), function(x) sum((results$posterior[,2] > x) == trainabs$interest, na.rm=TRUE)/nrow(trainabs))
+ggplot(data.frame()) + geom_point(aes(seq(0,1,.05), accuracy))
+sum((results$posterior[,2] > .76)==trainabs$interest, na.rm = TRUE)/nrow(trainabs) # 76.849%
+
 
 library("glmnet")
 scaled_input = scale(inputvars)
@@ -218,33 +196,40 @@ results = predict(model, newx = scaled_input,
                   s=model$lambda.min, type="response")
 library("pROC")
 roc(trainabs$interest, as.numeric(results), plot=TRUE)
-#0.874 ROC with words
-#0.9082 with bigrams
-#0.8943 with simplified version
+#0.9129 auc
 
 accuracy = sapply(seq(.2,.6,.01), function(x) sum((results > x) == trainabs$interest)/nrow(trainabs))
 ggplot(data.frame()) + geom_point(aes(x=seq(.2,.6,.01), y=accuracy))
 
-sum((results > .39) == trainabs$interest)/nrow(trainabs) # max words: accuracy 83.4%, half cutoff is 79.6^
-# bigrams: accuracy 84.85%, half cutoff is .8293
-# simplified is 0.8410384
+sum((results > .41) == trainabs$interest)/nrow(trainabs)
+# 85.4% accuracy
+
+# try this with test data, see below random forests for generating
+scaledtestinputvars = scale(testinputvars,
+                center = attr(scaled_input, "scaled:center"),
+                scale = attr(scaled_input, "scaled:scale"))
+results = predict(model, newx=scaledtestinputvars, type="response")
+roc(testabs$interest, as.numeric(results), plot=TRUE) # 0.881
+accuracy = sapply(seq(.1,.9,.01), function(x) sum((results > x) == testabs$interest)/nrow(testabs))
+ggplot(data.frame()) + geom_point(aes(x=seq(.1,.9,.01), y=accuracy), alpha=0.5) # 86.48% accuracy
 
 # try a random forest
 library("randomForest")
 model = randomForest(scaled_input, y=trainabs$interest, ntree=50)
 
 sum((model$predicted > .5) == trainabs$interest)/nrow(trainabs)
-# bigrams: wow, 82.4 acurracy without optimizing.
-# simplified is 82.9 accuracy
+#82%
 
-roc(trainabs$interest, model$predicted, plot=TRUE) # auc .8706
+roc(trainabs$interest, model$predicted, plot=TRUE) # auc .8656
 model$importance[order(model$importance),]
 # most important are neural, network, neep, covolut, train, task, learn, category-cs.NE, recurr, dataset, architectur....
+
+save(scaled_input, categories, authors, frequencies.sparse, file = "savestuff.rdata")
 
 library("caret")
 library("doParallel")
 #??do
-doMC::registerDoMC(cores=3)
+doMC::registerDoMC(cores=4)
 
 
 set.seed(114)
@@ -288,8 +273,8 @@ testauthors = sparseMatrix(i=i, j=j,
                            dims=c(nrow(testabs),ncol(authors)))
 colnames(testauthors) = colnames(authors)
 # check that this worked
-testabs$authorsaffil[84]
-testauthors[84,"Sergey Levine"]
+testabs$authorsaffil[53]
+testauthors[53,testauthors[53,]]
 
 # test categories
 testcatterms = str_match_all(testabs$categoryterms, "[{\",; ]((([A-Z])\\.[0-9]*)(?:\\.[0-9m]*)?)(?:\\.[0-9a-z])?(?=[ \",;}])|[{\",; ](((\\d\\d)[-A-Z])[0-9xX][0-9xX])(?=[ }\",;])|[{, ](([-a-zA-Z]*)(?:\\.[-a-zA-Z]*)?)(?=[ },])")
@@ -310,7 +295,8 @@ results = predict(caret_fit, newdata=testinputvars, type="prob")
 accuracy = sapply(seq(0,1,.01), function(x) sum((results[,1] < x) == testabs$interest)/nrow(testabs))
 accuracy2 = sapply(seq(0,1,.01), function(x) sum((results[,2] > x) == testabs$interest)/nrow(testabs))
 ggplot(data.frame()) + geom_point(aes(x=seq(0,1,.01), y=accuracy), alpha=0.5) + geom_point(aes(x=seq(0,1,.01)), y=accuracy2, color="red")
-# 80% accuracy at 0.5, great.
+# max 87.16% accuracy
+roc(testabs$interest, results[,1], plot=TRUE) # .8978
 
 # ways to improve: incorporate the date published
   # fiddle with the document term matrix, a la paul graham
